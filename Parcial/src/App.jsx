1ec36d8481fbd3d336d900ddcb50340de9e8936b
Juloja <j.lo.jaramillo@gmail.com>
2024-11-06 21:49:35 -0500

    Entregando el parcial +

--- Parcial/src/App.jsx
+++ Parcial/src/App.jsx
@@ -1,12 +1,64 @@
+import React, { useState } from "react";
 import Card from "./Card";
 
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const [pokemon, setPokemon] = useState("");
+  const [error, setError] = useState("");
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (nombre.trim().length < 3 || nombre.trim().startsWith(" ")) {
+      setError("ese no es tu nombre 🧐");
+      return;
+    }
+    if (pokemon.length >12) {
+      setError("Por favor, ingrese un pokemon válido, todos sabemos que tienen un nombre más corto que ese 🧐.");
+      return;
+    }
+
+    setError(null);
+
+    setSubmittedData({ nombre, pokemon });
+  };
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Pokemon Favorito</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nombre">Nombre completo:</label>
+          <input
+            type="text"
+            id="nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            placeholder="Ingresa tu nombre"
+        />
+        </div>
+
+        <div>
+          <label htmlFor="pokemon">Pokemon:</label>
+          <input
+            type="pokemon"
+            id="pokemon"
+            value={pokemon}
+            onChange={(e) => setPokemon(e.target.value)}
+            placeholder="Ingresa tu pokemon favorito"
+          /> 
+        </div>
+
+        <button type="submit">Enviar</button>
+      </form>
+
+      {error && <p style={{color:"red"}}className="error">{error}</p>}
+
+      {submittedData && !error && <Card data={submittedData} />}
+
     </div>
   );
 }
--- Parcial/src/Card.jsx
+++ Parcial/src/Card.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({data}) => {
+  return(
+    data.nombre && data.pokemon? (
+      <div className="card">
+        <h3>Mira esto 😲:</h3>
+        <p><strong>Hola </strong> {data.nombre}</p>
+        <p><strong>Tu pokemon favorito es: </strong> {data.pokemon}, ¡Qué cool!</p>
+      </div>
+    ) : (
+      <div className="card">
+        <h3>Por favor ingrese los datos</h3>
+      </div>
+    )
+  ) 
 };
 
 export default Card;
